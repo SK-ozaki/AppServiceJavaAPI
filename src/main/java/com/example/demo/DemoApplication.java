cc57265eb55b8926e695c1301cde055796d9f25c
尾崎 楓 <kaede_ozaki@serio.inc>
2024-02-05 13:50:08 +0900

    修正

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,8 @@
 package com.example.demo;
 
+import java.util.TimeZone;
+
+import org.joda.time.DateTime;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,22 +10,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.nio.file.Paths;
-import java.util.TimeZone;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import java.util.ArrayList;
-import java.util.List;
-
-import org.joda.time.DateTime;
-
 @SpringBootApplication
 @RestController
 public class DemoApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
@@ -37,73 +28,17 @@ public class DemoApplication {
 		return String.format("Hello %s!", name);
 	}
 
-	private String dbFile = "src\\main\\java\\com\\example\\demo\\datebase.json";
+	@GetMapping("/value")
+	public String Value(@RequestParam String key) {
 
-	// サイト一覧を取得
-	@GetMapping("/siteRecord")
-	public String SiteRecord(@RequestParam String key) throws JsonProcessingException, IOException {
-
-		ObjectMapper objectMapper = new ObjectMapper();
-		JsonNode json = objectMapper
-				.readTree(Paths.get(dbFile).toFile());
-
-		String jsonstr = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(json);
-
-		return jsonstr;
-	}
-
-	// サイト情報を追加
-	@GetMapping("/siteAdd")
-	public String SiteAdd(@RequestParam String url, @RequestParam String tag, @RequestParam String comment)
-			throws JsonProcessingException, IOException {
-
-		// 現在のデータベースを取得
-		String returnString = "";
-		ObjectMapper mapper = new ObjectMapper();
-		JsonSiteLists siteList;
-
-		try {
-
-			JsonNode json = mapper
-					.readTree(Paths.get(dbFile).toFile());
-			siteList = mapper.readValue(json.toString(), JsonSiteLists.class);
-
-		} catch (FileNotFoundException e) {
-			return "データベースを読み込めませんでした";
-		}
-
-		// テスト用タグ
-		List<String> tags = new ArrayList<String>();
-		tags.add("");
-
-		// データを追加
-		List<JsonSiteList> list = new ArrayList<JsonSiteList>();
-		JsonSiteList indata = new JsonSiteList(url, tags, comment);
-
-		//// 既存データをリスト化
-		siteList.getsites().forEach(s -> list.add(s));
-
-		//// 新規データを追加
-		list.add(indata);
-		siteList.setsites(list);
-		returnString = siteList.toString();
-
-		try {
-			// JSON文字列に変換
-			returnString = mapper.writeValueAsString(siteList);
-			// データベースを更新
-			mapper.writeValue(new File(dbFile), siteList);
-			return returnString;
-		} catch (JsonProcessingException e) {
-			e.printStackTrace();
-			return "error";
-		}
+		// 確認用環境変数"TEST_KeyValut_Secret_01"
+		return System.getenv(key);
 	}
 
 	@RequestMapping("/time")
 	public String time() {
 		DateTime nowDateTime = new DateTime();
 		TimeZone tz = TimeZone.getDefault();
-		return "タイムゾーン：" + tz.getID() + "   時刻：" + nowDateTime.toString();
+		return "タイムゾーン：" + tz.getID() + " 時刻：" + nowDateTime.toString();
 	}
 }
